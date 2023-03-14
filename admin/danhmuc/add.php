4540f965fbf14f7b682c27d467ab86769ec04736
vandataa <ndat165@gmail.com>
2023-03-14 08:59:50 +0700

    new

---
+++ admin/danhmuc/add.php
@@ -0,0 +1,30 @@
+<!-- Begin Page Content -->
+<div class="container-fluid">
+
+    <!-- Page Heading -->
+    <h1 class="h3 mb-2 text-gray-800">Danh mục</h1>
+    <!-- DataTales Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary"><a href="index.php?sp=new_sp"></a></h6>
+        </div>
+        <div class="card-body">
+            <div class="table-responsive">
+                
+                    <form action="index.php?sp=new_sp" method="post">
+                        <h6 class="m-0 font-weight-bold text-primary">Tên danh mục</h6>
+                        <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                            aria-label="Search" aria-describedby="basic-addon2">
+                        <button type="submit">Thêm mới</button>
+                    </form>
+                        
+            </div>
+        </div>
+    </div>
+
+</div>
+<!-- /.container-fluid -->
+<!-- /.container-fluid -->
+
+</div>
+<!-- End of Main Content -->
--- admin/index.php
+++ admin/index.php
@@ -4,8 +4,16 @@ include "./header.php";
 
 if(isset($_GET['sp'])){
     $sp = $_GET['sp'];
-    switch($sp)  {
-        case '':
+    switch($sp){
+        case 'list_dm':{
+            include_once './danhmuc/list.php';
+            break;
+        }
+        case 'new_dm':{
+
+            include_once './danhmuc/add.php';
+            break;
+        }
     }
 }else{
     include "./home.php";
