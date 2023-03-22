129d8327ed8d680adbf0b2a616718b6a81d92c09
vandataa <ndat165@gmail.com>
2023-03-22 23:11:20 +0700

    new

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php 
 include_once './page/header.php';
-include_once './page/home.php';
+
+
+if (isset($_GET['sp'])) {
+    $sp = $_GET['sp'];
+    switch ($sp) {
+    case 'login':
+        include './page/login_resign.php';
+        break;
+       
+}}
+else{
+    include_once './page/home.php'; 
+}
 include_once './page/footer.php';
