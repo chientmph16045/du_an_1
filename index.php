0f5d88d6d115a10b5a6168f9c54a86563be76db1
thien <dothien.dvt.2508@gmail.com>
2023-03-22 17:47:33 +0700

    update

--- index.php
+++ index.php
@@ -1,5 +1,23 @@
-
-<?php 
-include_once './page/header.php';
-include_once './page/home.php';
-include_once './page/footer.php';
+<?php
+include './shop-leftsidebar.php';
+include "./module/pdo.php";
+include "./module/sanpham.php";
+include "./module/danhmuc.php";
+if (isset($_GET['act']) && ($_GET['act']) !== "") {
+    $act = $_GET['act'];
+    switch ($act) {
+        case 'home':
+            include './page/home.php';
+            break;
+        case 'shop':
+            $listdm = list_dm();
+            include './shop-leftsidebar.php';
+            break;
+        case 'blog':
+            include './page/blog.php';
+            break;
+        case 'contact':
+            include './page/contact.php';
+            break;
+    }
+}
