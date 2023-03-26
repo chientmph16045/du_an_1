89f3d7da9ea9f05f24b1c3dad39eed898aa7a9ba
vandataa <ndat165@gmail.com>
2023-03-26 13:37:16 +0700

    update

--- index.php
+++ index.php
@@ -1,4 +1,3 @@
-
 <?php
 session_start();
 include_once './page/header.php';
@@ -20,19 +19,19 @@ if (isset($_GET['sp'])) {
             if (isset($_POST['login'])) {
                 $email = $_POST['email'];
                 $pass = $_POST['pass'];
-                $login = checkuser($email,$pass);
-                if(is_array($login)){
+                $login = checkuser($email, $pass);
+                if (is_array($login)) {
                     $_SESSION['user'] = $login;
                     $yourURL = "index.php";
                     echo ("<script>location.href =' $yourURL '</script>");
-                }else{
+                } else {
                     $thongbaoerro = 'Sai tài khoản hoặc mật khẩu';
                     include './page/login_resign.php';
                 }
             }
-            break; 
+            break;
         case 'resign':
-            if(isset($_POST['resign'])){
+            if (isset($_POST['resign'])) {
                 $name = $_POST['name'];
                 $email = $_POST['email'];
                 $pass = $_POST['pass'];
@@ -41,34 +40,62 @@ if (isset($_GET['sp'])) {
             }
             include './page/login_resign.php';
             break;
-        case 'forget' :
-            if(isset($_POST['forget'])){
-                $email =$_POST['email'];
+        case 'forget':
+            if (isset($_POST['forget'])) {
+                $email = $_POST['email'];
                 $check = checkforget($email);
                 if (is_array($check)) {
                     extract($check);
-                    $thongbao  = "Mật khẩu của bạn là " . $password;
+                    $thongbao = "Mật khẩu của bạn là " . $password;
                 } else {
                     $thongbao = 'Email không chính xác';
                 }
             }
             include './page/forget.php';
-            break;   
-            case 'chitiet':
-                include './page/single-product.php';
-                break;    
-            case 'account':
+            break;
+        case 'chitiet':
+            include './page/single-product.php';
+            break;
+        case 'account':
+            include './page/my-account.php';
+            break;
+        case 'account_fix':
+            if (isset($_POST['update']))
                 include './page/my-account.php';
-                break;  
-                case 'account_fix':
-                    if(isset($_POST['update']))
-                    include './page/my-account.php';
-                    break; 
+            break;
         case 'logout':
-                session_destroy();
-                $yourURL = "index.php";
-                echo ("<script>location.href =' $yourURL '</script>");
-                break;
+            session_destroy();
+            $yourURL = "index.php";
+            echo ("<script>location.href =' $yourURL '</script>");
+            break;
+
+        case 'home':
+            include './page/home.php';
+            break;
+        case 'shop':
+            if (isset($_POST['kyw']) && ($_POST['kyw']) != "") {
+                $kyw = $_POST['kyw'];
+            } else {
+                $kyw = "";
+            }
+            if (isset($_GET['idCate']) && ($_GET['idCate']) > 0) {
+                $idCate = $_GET['idCate'];
+            } else {
+                $idCate = 0;
+            }
+            $loadsp = list_sp($kyw, $idCate);
+            $listdm = list_dm();
+            include './page/shop-leftsidebar.php';
+            break;
+        case 'blog':
+            include './page/blog.php';
+            break;
+        case 'contact':
+            include './page/contact.php';
+            break;
+        default:
+            include './page/home.php';
+            break;
     }
 } else {
     include_once './page/home.php';
