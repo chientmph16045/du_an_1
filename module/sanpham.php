134d9f29457cdee72d32f9c830abf9071f2362d0
thien <dothien.dvt.2508@gmail.com>
2023-03-26 13:29:46 +0700

    thien

--- module/sanpham.php
+++ module/sanpham.php
@@ -6,30 +6,35 @@ function insert_sp($name, $price, $image, $description, $quantity, $idCategory)
     pdo_execute($sql);
 }
 
-function list_sp(){
+function list_sp($kyw, $idCate)
+{
     $sql = "SELECT * FROM `product`";
     $listsp = pdo_query($sql);
     return $listsp;
 }
-function load_one_sp($id){
-    $sql = "SELECT * FROM `product` WHERE idProduct = ".$_GET['id'];
+function load_one_sp($id)
+{
+    $sql = "SELECT * FROM `product` WHERE idProduct = " . $_GET['id'];
     $sp = pdo_query_one($sql);
     return $sp;
 }
-function load_one_list_sp_same($sp){
-    $sql = "SELECT * FROM `product` WHERE idCate = ".$sp;
+function load_one_list_sp_same($sp)
+{
+    $sql = "SELECT * FROM `product` WHERE idCate = " . $sp;
     $sp = pdo_query($sql);
     return $sp;
 }
-function update_sp($id,$name, $price, $image, $description, $quantity, $idCate){
-    if($image != ''){
-    $sql = "UPDATE `product` set `name`='".$name."', `price`='".$price."',`image`='".$image."',`description`='".$description."',`quantity`='".$quantity."',`idCate`='".$idCate."' WHERE `product`.`idProduct`=".$id;
-    }else{
-    $sql = "UPDATE `product` set `name`='".$name."', `price`='".$price."',`description`='".$description."',`quantity`='".$quantity."',`idCate`='".$idCate."' WHERE `product`.`idProduct`=".$id;
+function update_sp($id, $name, $price, $image, $description, $quantity, $idCate)
+{
+    if ($image != '') {
+        $sql = "UPDATE `product` set `name`='" . $name . "', `price`='" . $price . "',`image`='" . $image . "',`description`='" . $description . "',`quantity`='" . $quantity . "',`idCate`='" . $idCate . "' WHERE `product`.`idProduct`=" . $id;
+    } else {
+        $sql = "UPDATE `product` set `name`='" . $name . "', `price`='" . $price . "',`description`='" . $description . "',`quantity`='" . $quantity . "',`idCate`='" . $idCate . "' WHERE `product`.`idProduct`=" . $id;
     }
     pdo_execute($sql);
 }
-function delete_sp($id){
+function delete_sp($id)
+{
     $sql = "DELETE FROM `product` WHERE idProduct = " . $_GET['id'];
     pdo_execute($sql);
 }
