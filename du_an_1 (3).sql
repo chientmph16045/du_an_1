141e83c9c094649f3ee1e361ac2dfd10280e9830
vandataa <ndat165@gmail.com>
2023-04-01 07:20:02 +0700

    update

--- du_an_1 (3).sql
+++ du_an_1 (3).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th3 30, 2023 lúc 03:31 AM
--- Phiên bản máy phục vụ: 10.4.21-MariaDB
--- Phiên bản PHP: 7.3.31
+-- Thời gian đã tạo: Th4 01, 2023 lúc 02:19 AM
+-- Phiên bản máy phục vụ: 10.4.27-MariaDB
+-- Phiên bản PHP: 8.2.0
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -34,17 +34,22 @@ CREATE TABLE `cart` (
   `quantity` int(11) NOT NULL,
   `tinhtrang` int(11) NOT NULL,
   `idSp` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `cart`
 --
 
 INSERT INTO `cart` (`idCart`, `idOrder`, `idUser`, `quantity`, `tinhtrang`, `idSp`) VALUES
-(1, 1, 1, 1, 1, 3),
-(2, 3, 1, 1, 1, 3),
-(3, 3, 1, 1, 1, 3),
-(4, 4, 12, 1, 1, 3);
+(1, 1, 1, 1, 3, 3),
+(2, 3, 1, 1, 0, 3),
+(3, 3, 1, 1, 7, 3),
+(4, 4, 12, 1, 1, 3),
+(5, 5, 1, 1, 3, 3),
+(7, 6, 1, 1, 6, 3),
+(8, 7, 1, 1, 0, 3),
+(9, 7, 1, 1, 0, 3),
+(10, 8, 13, 1, 1, 3);
 
 -- --------------------------------------------------------
 
@@ -55,7 +60,7 @@ INSERT INTO `cart` (`idCart`, `idOrder`, `idUser`, `quantity`, `tinhtrang`, `idS
 CREATE TABLE `category` (
   `idCategory` int(10) NOT NULL,
   `name` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `category`
@@ -79,7 +84,7 @@ CREATE TABLE `comment` (
   `dateComment` varchar(255) NOT NULL,
   `idProduct` int(10) NOT NULL,
   `idUser` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -92,19 +97,23 @@ CREATE TABLE `placinganorder` (
   `name` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `phone` int(11) NOT NULL,
-  `dateOder` varchar(255) NOT NULL,
+  `dateOder` date NOT NULL,
   `idbill` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `placinganorder`
 --
 
 INSERT INTO `placinganorder` (`idOder`, `name`, `address`, `phone`, `dateOder`, `idbill`) VALUES
-(1, 'dat', '1', 0, '1680137200', 0),
-(2, 'dat', '1', 0, '1680137323', 0),
-(3, 'dat', '123123', 2222, '1680137450', 0),
-(4, 'chiến tạ', 'hà nội', 1, '1680138061', 0);
+(1, 'dat', '1', 0, '0000-00-00', 0),
+(2, 'dat', '1', 0, '0000-00-00', 0),
+(3, 'dat', '123123', 2222, '0000-00-00', 0),
+(4, 'chiến tạ', 'hà nội', 1, '0000-00-00', 0),
+(5, 'Đạt coming', 'hà nội', 962096125, '0000-00-00', 0),
+(6, 'Đạt coming', 'hà nội az', 962096125, '0000-00-00', 0),
+(7, 'Đạt coming', 'hà nội az', 962096125, '0000-00-00', 0),
+(8, 'Nguyễn Văn Đạt', 'Mĩ Đình 2 ,Nguyễn Hoàng ,Nam Từ Liêm ,Hà Nội', 123123, '0000-00-00', 0);
 
 -- --------------------------------------------------------
 
@@ -114,19 +123,19 @@ INSERT INTO `placinganorder` (`idOder`, `name`, `address`, `phone`, `dateOder`, 
 
 CREATE TABLE `product` (
   `idProduct` int(10) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `namePro` varchar(255) NOT NULL,
   `price` double(10,2) NOT NULL,
   `image` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `quantity` int(11) NOT NULL,
   `idCate` int(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `product`
 --
 
-INSERT INTO `product` (`idProduct`, `name`, `price`, `image`, `description`, `quantity`, `idCate`) VALUES
+INSERT INTO `product` (`idProduct`, `namePro`, `price`, `image`, `description`, `quantity`, `idCate`) VALUES
 (3, 'M2K Tekno Platinum Tint Celery aaaaaa', 500.00, '264856015_1632391990436744_1873520468356436573_n.jpg', 'zegrgzccasca', 111, 2);
 
 -- --------------------------------------------------------
@@ -137,17 +146,22 @@ INSERT INTO `product` (`idProduct`, `name`, `price`, `image`, `description`, `qu
 
 CREATE TABLE `status` (
   `idStatus` int(11) NOT NULL,
-  `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL
+  `nameStatus` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `status`
 --
 
-INSERT INTO `status` (`idStatus`, `name`) VALUES
+INSERT INTO `status` (`idStatus`, `nameStatus`) VALUES
+(0, 'Hủy hàng thành công'),
 (1, 'Đang xác nhận'),
-(2, 'Đang giao hàng'),
-(3, 'Đã giao hàng');
+(2, 'Đã xác nhận đơn hàng'),
+(3, 'Đang giao hàng'),
+(4, 'Giao hàng thành công'),
+(5, 'Đã giao hàng'),
+(6, 'Đang xác nhận hủy hàng'),
+(7, 'Hủy hàng thất bại tiếp tục giao hàng');
 
 -- --------------------------------------------------------
 
@@ -157,23 +171,24 @@ INSERT INTO `status` (`idStatus`, `name`) VALUES
 
 CREATE TABLE `user` (
   `idUser` int(10) NOT NULL,
-  `name` varchar(255) NOT NULL,
+  `nameUser` varchar(255) NOT NULL,
   `address` varchar(255) NOT NULL,
   `phone` varchar(20) DEFAULT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `status` tinyint(50) NOT NULL DEFAULT 1,
   `role` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Đang đổ dữ liệu cho bảng `user`
 --
 
-INSERT INTO `user` (`idUser`, `name`, `address`, `phone`, `email`, `password`, `status`, `role`) VALUES
-(1, 'chiến tạ', 'hà nội', '0359269559', 'chien@gmail.com', '1', 1, '3'),
+INSERT INTO `user` (`idUser`, `nameUser`, `address`, `phone`, `email`, `password`, `status`, `role`) VALUES
+(1, 'Đạt coming', 'hà nội az', '0962096125', 'ndat165@gmail.com', '1', 1, '3'),
 (11, 'Tạ Minh Chiến', 'hà nội', '0359269559', 'chientmph@fpt.edu.vn', '1', 1, ''),
-(12, 'chiến tạ', 'hà nội', '0359269559', 'chien@gmail.com', '1', 1, '');
+(12, 'chiến tạ', 'hà nội', '0359269559', 'chien@gmail.com', '1', 1, ''),
+(13, 'Nguyễn Văn Đạt', 'Mĩ Đình 2 ,Nguyễn Hoàng ,Nam Từ Liêm ,Hà Nội', '123123', 'vandatpk01@gmail.com', '1', 1, '');
 
 --
 -- Chỉ mục cho các bảng đã đổ
@@ -236,7 +251,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT cho bảng `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `idCart` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idCart` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT cho bảng `category`
@@ -254,7 +269,7 @@ ALTER TABLE `comment`
 -- AUTO_INCREMENT cho bảng `placinganorder`
 --
 ALTER TABLE `placinganorder`
-  MODIFY `idOder` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `idOder` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT cho bảng `product`
@@ -266,13 +281,13 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT cho bảng `status`
 --
 ALTER TABLE `status`
-  MODIFY `idStatus` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `idStatus` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT cho bảng `user`
 --
 ALTER TABLE `user`
-  MODIFY `idUser` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `idUser` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- Các ràng buộc cho các bảng đã đổ
